69d11621e1d32bb633b635c30c38bdfde1ee47cd
Bruno Santos Vieira <brunosvieira88@gmail.com>
2021-08-06 21:19:05 -0300

    primeiro

---
+++ desenhos.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Desenhos personalizados</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	 <!-- Bootstrap CSS -->
+    <link href="node_modules/bootstrap/compiler/bootstrap.css" rel="stylesheet" >
+     <!-- estilo CSS -->
+	<link rel="stylesheet" type="text/css" href="estilos.css">
+</head>
+</head>
+<body>
+
+<div >
+
+	   <nav class="navbar navbar-expand-lg navbar-light bg-light">
+	    <form class="d-flex" style="padding: 10px; margin-right: 10px;">
+	        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+	        <button class="btn btn-outline-success" type="submit">Search</button>
+	    </form>
+	    <a href="teste.php" class="navbar-brand">Pokémon</a>
+
+	    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSite">
+	      <span class="navbar-toggler-icon"></span>
+	    </button>
+
+	    <div class="collapse navbar-collapse" id="navbarSite">
+
+	      <ul class="navbar-nav">
+
+	        <li class="nav-item">
+	          <a  class="nav-link "href="#">Inicio</a>
+	        </li>
+	        <li class="nav-item">
+	          <a  class="nav-link "href="#">Jogos</a>
+	        </li>
+	        <li class="nav-item dropdown">
+	          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+	            Pokemons
+	          </a>
+	          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+	            <li><a class="dropdown-item" href="#">Geração 1</a></li>
+	            <li><a class="dropdown-item" href="#">Geração 2 </a></li>
+	            <li><hr class="dropdown-divider"></li>
+	            <li><a class="dropdown-item" href="#">Mais Gerações</a></li>
+	          </ul>
+	        </li>
+	      </ul>
+	    </nav>
+    </div>
+
+<section class="ml-2  mt-4">
+	<div class="row">
+
+		<div class=" col-xs-12 col-md-4">
+			<img src="TREINADOR BRUNO E ESPOSA - ARTE FINAL.png"  style="width:70%;border: 1px solid black;margin-left: 60px;box-shadow: 1px 1px 2px  black;">
+		</div>
+
+		<div class=" col-xs-12 col-md-4">
+			<img src="TREINADOR BRUNO E ESPOSA - ARTE FINAL.png"  style="width:70%;border: 1px solid black;margin-left: 60px;box-shadow: 1px 1px 2px  black;">
+		</div>
+
+		<div class=" col-xs-12 col-md-4">
+			<img src="TREINADOR BRUNO E ESPOSA - ARTE FINAL.png"  style="width:70%;border: 1px solid black;margin-left: 60px;box-shadow: 1px 1px 2px  black;">
+		</div>
+	
+	
+	</div>
+</section>
+
+<section class="ml-2  mt-4">
+	<div class="row">
+
+		<div class=" col-xs-12 col-md-4">
+			<img src="TREINADOR BRUNO E ESPOSA - ARTE FINAL.png"  style="width:70%;border: 1px solid black;margin-left: 60px;box-shadow: 1px 1px 2px  black;">
+		</div>
+
+		<div class=" col-xs-12 col-md-4">
+			<img src="TREINADOR BRUNO E ESPOSA - ARTE FINAL.png"  style="width:70%;border: 1px solid black;margin-left: 60px;box-shadow: 1px 1px 2px  black;">
+		</div>
+
+		<div class=" col-xs-12 col-md-4">
+			<img src="TREINADOR BRUNO E ESPOSA - ARTE FINAL.png"  style="width:70%;border: 1px solid black;margin-left: 60px;box-shadow: 1px 1px 2px  black;">
+		</div>
+	
+	
+	</div>
+</section>
+
+<section class="ml-2  mt-4">
+	<div class="row">
+
+		<div class=" col-xs-12 col-md-4">
+			<img src="TREINADOR BRUNO E ESPOSA - ARTE FINAL.png"  style="width:70%;border: 1px solid black;margin-left: 60px;box-shadow: 1px 1px 2px  black;">
+		</div>
+
+		<div class=" col-xs-12 col-md-4">
+			<img src="TREINADOR BRUNO E ESPOSA - ARTE FINAL.png"  style="width:70%;border: 1px solid black;margin-left: 60px;box-shadow: 1px 1px 2px  black;">
+		</div>
+
+		<div class=" col-xs-12 col-md-4">
+			<img src="TREINADOR BRUNO E ESPOSA - ARTE FINAL.png"  style="width:70%;border: 1px solid black;margin-left: 60px;box-shadow: 1px 1px 2px  black;">
+		</div>
+	
+	
+	</div>
+</section>
+	
+
+</body>
+<script src="jquery.js"></script>
+<script type="text/javascript" src="animation.fix.js"></script>
+    <script src="node_modules/jquery/dist/jquery.js"></script>
+    <script src="node_modules/popper.js/dist/umd/popper.js"></script>
+    <script src="node_modules/bootstrap/dist/js/bootstrap.js"></script>
+</html>
