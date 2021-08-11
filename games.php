b31808a20e62e9e9b52cdb90c4004b364380c931
cknakayama <ckn14oz@hotmail.com>
2021-08-11 14:14:49 -0300

    criando games.php

---
+++ games.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+	<title></title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	 <!-- Bootstrap CSS -->
+    <link href="node_modules/bootstrap/compiler/bootstrap.css" rel="stylesheet" >
+
+</head>
+<body>
+
+<header>
+	
+	<div >
+
+	   <nav class="navbar navbar-expand-lg navbar-light bg-light">
+	    <form class="d-flex" style="padding: 10px; margin-right: 10px;">
+	        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+	        <button class="btn btn-outline-success" type="submit">Search</button>
+	    </form>
+	    <a href="#" class="navbar-brand">Pokémon</a>
+
+	    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSite">
+	      <span class="navbar-toggler-icon"></span>
+	    </button>
+
+	    <div class="collapse navbar-collapse" id="navbarSite">
+
+	      <ul class="navbar-nav">
+
+	        <li class="nav-item">
+	          <a  class="nav-link "href="#">Inicio</a>
+	        </li>
+	        <li class="nav-item">
+	          <a  class="nav-link "href="#">Jogos</a>
+	        </li>
+	        <li class="nav-item dropdown">
+	          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+	            Pokemons
+	          </a>
+	          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+	            <li><a class="dropdown-item" href="#">Geração 1</a></li>
+	            <li><a class="dropdown-item" href="#">Geração 2 </a></li>
+	            <li><hr class="dropdown-divider"></li>
+	            <li><a class="dropdown-item" href="#">Mais Gerações</a></li>
+	          </ul>
+	        </li>
+	      </ul>
+	    </nav>
+    </div>
+
+    <div id="logo_header" class="align-itens-center">
+				<h1>Jogos da Série Principal</h1>
+				<div>
+					<img class="mt-4 mb-4" src="imagens/pikachu.png" style="width:130px;border-radius:70%;">
+				</div>
+				
+			</div>
+
+</header>
+    <h1></h1>
+        <p>Os jogos da série principal de Pokémon são separados por geração. Cada geração traz uma nova região, novos pokémons e novas mecânicas.</p>
+        <p>É de costume da Gamefreak lançar em cada geração no mínimo 2 jogos simultâneos, com basicamente duas diferenças, pokémons e lendários exclusivos de cada versão.</p>
+        <p>Vamos dar uma olhada então nesses jogos e, se tiver curiosidade de ver todos os pokémons de cada geração, acesse nossa pokédex!</p>
+        <p>OBS: Pokémons de uma geração podem aparecer em outras regiões também!</p>
+    <h2>1&ordm; GERAÇÃO</h2>
+
+
+</body>
+</html>
