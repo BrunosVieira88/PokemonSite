419205b225d14011fab1b60735000c029af5bcd7
Cristiano Kazuaki Nakayama <68041502+cknakayama@users.noreply.github.com>
2021-08-17 16:23:24 -0300

    conteudo sobre jogos gen 1, 2 e 3

--- games.php
+++ games.php
@@ -60,11 +60,77 @@
 
 </header>
     <h1></h1>
-        <p>Os jogos da série principal de Pokémon são separados por geração. Cada geração traz uma nova região, novos pokémons e novas mecânicas.</p>
-        <p>É de costume da Gamefreak lançar em cada geração no mínimo 2 jogos simultâneos, com basicamente duas diferenças, pokémons e lendários exclusivos de cada versão.</p>
-        <p>Vamos dar uma olhada então nesses jogos e, se tiver curiosidade de ver todos os pokémons de cada geração, acesse nossa pokédex!</p>
-        <p>OBS: Pokémons de uma geração podem aparecer em outras regiões também!</p>
-    <h2>1&ordm; GERAÇÃO</h2>
+        <p>Os jogos da série principal de Pokémon são separados por geração. Cada geração traz uma nova região, novos pokémons e 
+			novas mecânicas.<br>
+        É de costume da Gamefreak lançar em cada geração no mínimo 2 jogos simultâneos, com basicamente duas diferenças, pokémons 
+		e lendários exclusivos de cada versão.<br>
+        Vamos dar uma olhada então nesses jogos e, se tiver curiosidade de ver todos os pokémons de cada geração, acesse nossa pokédex!<br>
+        OBS: Pokémons de uma geração podem aparecer em outras regiões também!</p>
+	<hr/>
+    <h2>1 - 1&ordm; GERAÇÃO(1996 - 1999, 2004 e 2018)</h2>
+	<h3>1.1 - Pokémon Red, Green, Blue e Yellow:</h3>
+		<p>Os jogos originais de Pokémon são japoneses com um elemento de estratégia e foram criados por Satoshi Tajiri para o console Game Boy. 
+			Estes jogos de RPGs, e suas sequências, recriações e traduções para o inglês, ainda são considerados os jogos "principais" de 
+			Pokémon, e os jogos com os quais a maioria dos fãs da série estão familiarizados.</p>
+		<p>A série Pokémon começou com o lançamento de Pocket Monsters Red e Green para o Game Boy no Japão. Quando estes jogos se revelaram 
+			extremamente populares, uma versão melhorada Blue foi lançada algum tempo depois, e a versão Blue foi reprogramada como 
+			Pokémon Red & Blue para lançamento internacional. A versão original "Green" nunca foi lançada fora do Japão. Depois, uma segunda 
+			versão melhorado, Pokémon Yellow, foi lançado para usar a paleta de cores do Game Boy Color e mais uma semelhança estilística com 
+			o anime popular Pokémon. Esta primeira geração de jogos introduziu as 151 espécies originais de Pokémon, bem como os conceitos 
+			básicos do jogo de capturar, treinar, lutar e trocar Pokémon com jogadores humanos de computador. Essas versões dos jogos ocorrem 
+			dentro da região fictícia Kanto, embora o nome "Kanto" não tenha sido usado até a segunda geração.</p>
+	<h3>1.2 - Pokémon FireRed e LeafGreen:</h3>
+		<p>Em 2004, Pokémon Red & Green ganharam remakes para o Game Boy Advance e foram lançados como Pokémon FireRed & LeafGreen. 
+			FireRed & LeafGreen voltava ao cenário dos primeiros jogos e refazia a jornada deles. Com isso, trazia a possibilidade de 
+			obtenção de todos os 150 primeiros e também um aspecto de nostalgia, sendo refeitos quase que exatamente como os primeiros 
+			jogos. Os remakes também traziam uma nova área para o mundo Pokémon: as Sevii Islands, onde era possível achar e capturar 
+			monstrinhos que eram nativos de Johto, palco dos jogos de segunda geração. Foram os jogos mais vendidos da Nintendo entre 
+			2004 e 2005.</p>
+	<h3>1.3 - Let´s Go, Pikachu! e Let´s Go Eevee! </h3>
+		<p>Em 29 de maio de 2018, foi anunciado dois novos jogos na franquia principal, Pokémon Let's Go, Pikachu! & Pokémon Let's Go, Eevee!. 
+			Eles são remakes de Pokémon Yellow com mecânica de jogo emprestada de Pokémon GO e foram lançados mundialmente no Nintendo Switch 
+			em 16 de novembro de 2018.</p>
+	<hr/>
+	<h2>2 - 2&ordm; GERAÇÃO(1999 - 2002, 2009)</h2>
+	<h3>2.1 - Pokémon Gold, Silver e Crystal:</h3>
+		<p>A segunda geração de jogos eletrônicos de "Pokémon" começou em 1999 com o lançamento do Pokémon Gold' & Silver para o console 
+			Game Boy Color, com Austrália e América do Norte recebendo o jogo em outubro de 2000 e data de lançamento europeu de abril de 2001. 
+			Como na geração anterior, uma versão aprimorada chamada Pokémon Crystal foi lançada mais tarde.</p>
+		<p>Esta geração introduziu 100 novas espécies de Pokémon, para um total de 251 Pokémon para coletar, treinar e lutar. Novos recursos de 
+			jogabilidade incluem um sistema de dia e noite (refletindo a hora do dia no mundo real) que influencia eventos no jogo, uso completo 
+			da paleta de cores do Game Boy Color, uma interface melhorada e um sistema de inventário atualizado, melhor equilíbrio na coleção de 
+			Pokémon e seus movimentos, estatísticas e itens equipáveis ​​(uma nova adição), a adição de dois novos tipos Pokémon (Dark e Steel) para 
+			melhor equilibrar as forças e fraquezas de cada Pokémon, Reprodução de Pokémon e uma nova região chamada Johto. Depois de explorar Johto, 
+			o jogador pode viajar para o leste para explorar a região adjacente, a região de Kanto.</p>
+	<h3>2.2 - Pokémon HeartGold e SoulSilver:</h3>
+		<p>Em 12 de setembro de 2009 foi colocado à venda no Japão, remakes dos jogos Pokémon Gold & Silver para o Nintendo DS intitulado 
+			Pokémon HeartGold & SoulSilver. Assim como Pokémon Yellow, que somente Pikachu andava fora da Poké-bola, qualquer Pokémon que seja o primeiro 
+			de sua equipe anda ao seu lado agora. E duas novas áreas para Johto, uma espécie de pokémon Safari e também traz de volta o Battle Frontier. 
+			Vem acompanhando também o Pokéwalker.</p>
+	<hr/>
+	<h2>3 - 3&ordm; GERAÇÃO(2002 - 2006, 2014)</h2>
+	<h3>3.1 - Pokémon Ruby, Sapphire e Esmerald:</h3>
+		<p>Em Março de 2002, foram lançados os primeiros jogos da terceira geração, mais conhecida pelos jogadores como geração Advance, já que seus RPGs 
+			originais eram feitos para o portátil Game Boy Advance. Pokémon Ruby & Sapphire, que ultrapassaram as 3,1 milhões de unidades vendidas, 
+			sendo assim os mais comercializados no Japão em 2002, traziam 135 novos Pokémon, mas era impossível transferir Pokémon de gerações anteriores 
+			para os jogos, pois o treinamento deles em Pokémon Ruby & Sapphire se baseava em um outro sistema, o de Effort Values (valores de esforço), 
+			pontos que, ao serem bem calculados e distribuídos ao Pokémon, deixavam-no mais forte que outros treinados sem esse método.</p>
+		<p>Em 2005, a geração Advance termina com Pokémon Emerald. Emerald era uma versão complementar para Ruby & Sapphire e trazia novos recursos, 
+			como o retorno das animações dos Pokémon ao início das batalhas, conceito já usado em Pokémon Crystal, uma nova área repleta de desafios 
+			chamada Battle Frontier e também a possibilidade, através de eventos, de capturar Mew sem a necessidade de truques, como em Red, Blue e Yellow. 
+			Foi o segundo jogo mais vendido do Japão em 2005.</p>
+	<h3>Pokémon Omega Ruby e Alpha Sapphire:</h3>
+		<p>A Nintendo anunciou oficialmente os jogos em 7 de maio de 2014, através de um comunicado de imprensa. Omega Ruby e Alpha Sapphire foram lançados 
+			no Japão e nas Américas em 21 de novembro e ao resto da Europa em 28 de novembro de 2014. A única informação detalhada divulgada foi que os novos 
+			jogos "conduzirão os jogadores através de uma história dramática dentro de um mundo novo e espetacular". Após o anúncio, não ficou claro se 
+			Omega Ruby e Alpha Sapphire seriam realmente remakes ou novos jogos, devido a descrição do comunicado de imprensa. Então, em uma nova coletiva, 
+			o presidente da Nintendo, Satoru Iwata, confirmou que os novos jogos são remakes completos das versões anteriores de 2002.</p>
+		<p>Durante o vídeo ao vivo da Nintendo na Electronic Entertainment Expo 2014 em 10 de junho de 2014 (que mais tarde foi produzido em uma versão 
+			impressa na CoroCoro Comic do Japão), um trailer para Omega Ruby e Alpha Sapphire foi divulgado, exibindo novas cenas dos jogos, incluindo o 
+			novo design dos personagens e as novas Mega Evoluções para Sceptile e Swampert, que não foram mostradas em X e Y, como no caso de Blaziken. 
+			Um comunicado de imprensa da Nintendo deu a entender que as outras pedras Megas (itens do jogo necessários para a Mega Evolução) só poderiam ser 
+			encontrado na região de Hoenn em Omega Ruby e Alpha Sapphire. Também foi divulgado no site oficial as novas formas de Groudon e Kyogre, que são 
+			destaques na arte da capa dos jogos, e essas mudanças supostamente estariam relacionadas a Mega Evolução.</p>
 
 
 </body>
