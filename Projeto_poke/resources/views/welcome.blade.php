330cece6756e913c9d5eb9690941ebd058dc8afd
Bruno Santos Vieira <75911878+BrunosVieira88@users.noreply.github.com>
2022-03-29 14:52:53 -0300

    criado projeto

---
+++ Projeto_poke/resources/views/welcome.blade.php
@@ -0,0 +1,181 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	 <!-- Bootstrap CSS -->
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+     <!-- estilo CSS -->
+	<link rel="stylesheet" type="text/css" href="estilos.css">
+</head>
+<body >
+
+<header>
+	
+	<div >
+
+	   <nav class="navbar navbar-expand-lg navbar-light bg-light">
+	    <form class="d-flex" style="padding: 10px; margin-right: 10px;">
+	        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+	        <button class="btn btn-outline-success" type="submit">Search</button>
+	    </form>
+	    <a href="#" class="navbar-brand">Pokémon</a>
+
+	    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSite">
+	      <span class="navbar-toggler-icon"></span>
+	    </button>
+
+	    <div class="collapse navbar-collapse" id="navbarSite">
+
+	      <ul class="navbar-nav">
+
+	        <li class="nav-item">
+	          <a  class="nav-link "href="#">Inicio</a>
+	        </li>
+	        <li class="nav-item">
+	          <a  class="nav-link "href="#">Jogos</a>
+	        </li>
+	        <li class="nav-item dropdown">
+	          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+	            Pokemons
+	          </a>
+	          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+	            <li><a class="dropdown-item" href="#">Geração 1</a></li>
+	            <li><a class="dropdown-item" href="#">Geração 2 </a></li>
+	            <li><hr class="dropdown-divider"></li>
+	            <li><a class="dropdown-item" href="#">Mais Gerações</a></li>
+	          </ul>
+	        </li>
+	      </ul>
+	    </nav>
+    </div>
+     
+
+
+		<div id="logo_header" class="align-itens-center">
+			<h1>Pokémon </h1>
+			<div>
+				<img class="mt-4 mb-4" src="imagens/pikachu.png" style="width:130px;border-radius:70%;">
+			</div>
+			
+			<a href="#cadastro"><button  type="submit"  class="btn btn-primary btn-lg">Gotta Catch 'Em All!</button></a>
+		</div>
+					
+</header>
+<section class="container">
+	<div class="row">
+		<div class="col-12 mt-4">
+			<h1>Nosso jogo</h1>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus tincidunt euismod augue id aliquet. Cras lacus magna, tempor quis lorem sit amet, pharetra tempus felis. Donec gravida massa nec dui ultricies pharetra. Nullam fermentum sed magna et egestas. Fusce accumsan metus eu diam pulvinar sagittis. Sed suscipit placerat vulputate. Cras luctus est tortor, nec gravida ex tempor id. Praesent et semper lectus. Sed venenatis consectetur ipsum id sollicitudin. In ut euismod magna. Duis tempor ipsum arcu, et condimentum urna posuere vitae. Etiam convallis finibus blandit. Praesent fermentum laoreet ultricies. Mauris porta ac tellus vel bibendum. Aenean commodo, mi in blandit hendrerit, sapien tortor pharetra risus, eget laoreet felis elit in velit.
+			
+		</div>
+		
+	</div>
+	<div>
+		<div class="row">
+		
+			<div class="col-xs-12 col-md-6 mt-4" >
+				<h1>Jogos</h1>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				<!-- Button trigger modal -->
+				<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+				  Launch demo modal
+				</button>
+			</div>
+
+			<div class="col-xs-12 col-md-6 mt-4" >
+				<h1>Iniciais</h1>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				<!-- Button trigger modal -->
+				<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+				  Launch demo modal
+				</button>
+			</div>
+
+		</div>
+
+		<div class="row">
+		
+			<div class="col-xs-12 col-md-6 mt-2 mb-4" >
+				<h1>Tipos</h1>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				<!-- Button trigger modal -->
+			<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+			  Launch demo modal
+			</button>
+			</div>
+
+			<div class="col-xs-12 col-md-6 mt-2 mb-4" >
+				<h1>Visão</h1>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				<!-- Button trigger modal -->
+				<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+				  Launch demo modal
+				</button>
+			</div>
+
+		</div>
+	</div>
+
+<footer>
+	<div class="row">
+		<div class="col-xs-12 col-md-6">
+			<img src="imagens/TREINADOR BRUNO E ESPOSA - ARTE FINAL.png" class="img-responsive" style="width:70%;">
+		</div>
+		<div class="col-xs-12 col-md-6">
+			<h1>Desenhos personalizados</h1>
+
+			Quem nunca sonhou em ser um mestre pokemon ter seus proprios pokemons sua equipe e se tornar o maior campão da liga pokemon.
+			Gabriel torna os sonhos das pessoas possiveis atraves de desenhos personalizados onde voce pode finalmente se tornar um mestre pokemon. 
+
+			<a href="desenhos.php">Clique aqui</a>
+		</div>
+	</div>
+</footer>
+</section>
+	
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Jogos Pokemon</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+      	<div class="row mt-2" style="border-bottom: 1px solid black;">
+      		<div class="col-4">
+      			<img src="imagens/greenversion.jpg" style="width:70%;" >
+      		</div>
+      		<div class="col-8">
+      			Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      		</div>
+      	</div>
+
+  		<div class="row mt-2" style="border-bottom: 1px solid black;">
+      		<div class="col-4">
+      			<img src="imagens/greenversion.jpg" style="width:70%;" >
+      		</div>
+      		<div class="col-8">
+      			Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      		</div>
+      	</div>
+       
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Fechar</button>
+       
+      </div>
+    </div>
+  </div>
+</div>
+</body>
+
+
+    <script src="node_modules/jquery/dist/jquery.min.js"></script>
+    <script src="node_modules/popper.js/dist/umd/popper.min.js"></script>
+    <script src="node_modules/bootstrap/dist/js/bootstrap.js"></script>
+</html>
